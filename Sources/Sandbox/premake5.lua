194de1e7808229327080054021f658aeea26227d
mishabuhalo <bukhalo1999@gmail.com>
2021-03-19 20:21:39 +0200

    SFML setup

--- Sources/Sandbox/premake5.lua
+++ Sources/Sandbox/premake5.lua
@@ -1,5 +1,6 @@
 project "Sandbox"
     language "C++"
+    cppdialect (CPPDIALECT)
     rtti (RTTI)
     targetdir (TARGET_DIR)
     objdir (OUTPUT_DIR .. "/%{prj.name}")
@@ -8,17 +9,48 @@ project "Sandbox"
 
     includedirs
     {
-        INCLUDE_PATHS.ROOT
+        INCLUDE_PATHS.ROOT,
+        INCLUDE_PATHS.SFML
     }
 
-    defines { "IMGUI_DISABLE_OBSOLETE_FUNCTIONS" }
+    libdirs
+    {
+        LIB_PATHS.SFML_EXTLIB
+    }
 
+    links
+    {
+        "opengl32",
+        "openal32",
+        "freetype",
+        "winmm",
+        "gdi32",
+        "flac",
+        "vorbisenc",
+        "vorbisfile",
+        "vorbis",
+        "ogg",
+        "ws2_32"
+    }
+
+    defines { "SFML_STATIC", "IMGUI_DISABLE_OBSOLETE_FUNCTIONS" }
 
     filter "configurations:Debug"
         kind "ConsoleApp"
         defines { "DEBUG" }
         symbols "On"
         runtime "Debug"
+        libdirs 
+        {
+            LIB_PATHS.SFML_DEBUG
+        }
+        links 
+        {
+            "sfml-graphics-s-d",
+            "sfml-window-s-d",
+            "sfml-system-s-d",
+            "sfml-audio-s-d"
+        }
 
     filter "configurations:Release"
         kind "ConsoleApp"
@@ -26,11 +58,34 @@ project "Sandbox"
         optimize "On"
         symbols "On"
         runtime "Release"
+        libdirs 
+        {
+            LIB_PATHS.SFML_RELEASE
+        }
+        links
+        {
+            "sfml-graphics-s",
+            "sfml-window-s",
+            "sfml-system-s",
+            "sfml-audio-s",
+        }
 
         
     filter "configurations:Retail"
         kind "WindowedApp"
         defines { "NDEBUG", "RETAIL"}
         optimize "On"
-        symbols "On"
+        symbols "Off"
         runtime "Release"
+        libdirs 
+        {
+            LIB_PATHS.SFML_RETAIL
+        }
+        links
+        {
+            "sfml-graphics-s",
+            "sfml-window-s",
+            "sfml-system-s",
+            "sfml-audio-s",
+            "sfml-main"
+        }
--- Sources/premake5.lua
+++ Sources/premake5.lua
@@ -1,30 +1,39 @@
 workspace "M42-Engine"
-   configurations { "Debug", "Release", "Retail"}
-   architecture "x64"
-   startproject "Sandbox"
-   platforms { "Win64" }
+    configurations { "Debug", "Release", "Retail"}
+    architecture "x64"
+    startproject "Sandbox"
+    platforms { "Win64" }
+ 
+    flags { "MultiProcessorCompile" , "FatalWarnings", "NoPCH"}
+ 
+    CPPDIALECT = "C++latest"
+    RTTI = "Off"
+ 
+    TARGET_DIR = "%{wks.location}/../bin/%{cfg.buildcfg}-%{cfg.platform}-%{cfg.architecture}"
+    OUTPUT_DIR = "%{wks.location}/../bin/obj/%{cfg.buildcfg}-%{cfg.platform}-%{cfg.architecture}"
+ 
+    INCLUDE_PATHS = 
+    {
+        SFML = "%{wks.location}/SFML/Sources/include",
+        ROOT = "%{wks.location}"
+    }
+ 
+    LIB_PATHS = 
+    {
+        SFML_DEBUG = "%{wks.location}/SFML/Build/lib/Debug",
+        SFML_RELEASE = "%{wks.location}/SFML/Build/lib/RelWithDebInfo",
+        SFML_RETAIL = "%{wks.location}/SFML/Build/lib/Release"
+    }
+    
+    filter "action:vs*"
+        LIB_PATHS.SFML_EXTLIB = "%{wks.location}/SFML/Sources/extlibs/libs-msvc-universal/x64"
+        buildoptions {"/Zc:__cplusplus"}
+ 
+    filter "system:windows"
+        systemversion "latest"
 
-   flags { "MultiProcessorCompile" , "FatalWarnings", "NoPCH"}
-
-   RTTI = "Off"
-
-   TARGET_DIR = "%{wks.location}/../bin/%{cfg.buildcfg}-%{cfg.platform}-%{cfg.architecture}"
-   OUTPUT_DIR = "%{wks.location}/../bin/obj/%{cfg.buildcfg}-%{cfg.platform}-%{cfg.architecture}"
-
-   INCLUDE_PATHS = 
-   {
-      ROOT = "%{wks.location}"
-   }
-
-   LIB_PATHS = 
-   {
-
-   }
-
-   filter "action:vs*"
-      buildoptions {"/Zc:__cplusplus", "/std:c++latest"}
-
-   filter "system:windows"
-      systemversion "latest"
-
-   include "Sandbox"
+    postbuildcommands {
+        "{COPY} %{wks.location}/SFML/Sources/extlibs/bin/x64 " .. TARGET_DIR
+    }
+ 
+    include "Sandbox"
